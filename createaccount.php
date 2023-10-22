3feb9792352343839d47dfcef575e4bb07c18ec2
Charbel tannous <69799661+charbel30@users.noreply.github.com>
2023-10-22 20:26:18 +0000

    Update createaccount.php

--- createaccount.php
+++ createaccount.php
@@ -66,7 +66,7 @@ if (isset($_POST['submit'])) {
     <header>
         <logo>
 
-            <a href="home.html"> <img src="logo.png" alt="logo"></a>
+            <a href="index.html"> <img src="logo.png" alt="logo"></a>
             <div class="date_time"></div>
           
         </logo>
