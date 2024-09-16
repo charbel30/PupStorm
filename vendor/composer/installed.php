205b871ce7c51b4cc399de6b83d33ef8e3918b11
charbel30 <charbeltannous30@gmail.com>
2024-09-15 21:26:51 -0400

    Update composer.json and composer.lock files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pupstorm/pet-adoption-website',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e2fa9b99d1d477113c5fa8e68dd73de0a045de8b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pupstorm/pet-adoption-website' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e2fa9b99d1d477113c5fa8e68dd73de0a045de8b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
